b32918b6378511586e79b429418c45874aafe0d9
Alexander Bayer <alexanderbayer@posteo.de>
2022-11-29 09:38:15 +0100

    Update Media Queries

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -28,23 +28,23 @@ const GlobalStyle = createGlobalStyle`
   font-weight: bold;
   src: local("robotoBold"),
   url(${robotoBold});
+}
+body {
+  margin: auto;
+  -moz-osx-font-smoothing: grayscale;
+  background-color: var(--color-foam);
+  color: var(--color-granite);
+  width: 30rem;
+  font-family: "Roboto";
+  border: 1px solid var(--color-water-10);
+  padding: 0.3rem;
+}
 
 @media only screen and (max-width: 600px) {
   body {
     width: 90%;
     padding: 0.5rem;
   }
-}
-
-body {
-  margin: auto;
-  -moz-osx-font-smoothing: grayscale;
-  background-color: var(--color-foam);
-  color: var(--color-granite);
-  width: 20rem;
-  font-family: "Roboto";
-  border: 1px solid var(--color-water-10);
-  padding: 0.3rem;
 }
 `;
 
